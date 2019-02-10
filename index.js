7a3d8480c1eef094d51473ffada04a597d30018c
Evgeniy Poznyak <ek35mm@gmail.com>
2019-02-10 01:42:44 -0600

    genres api

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 
+app.use(express.json());
 app.use('/api/genres', genres);
+
+app.get('/', (req, res) => {
+  return res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'http://localhost';
+app.listen(port, () => console.log(`Listening: ${host}:${port}`));
--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,55 @@
 const express = require('express');
 router = express.Router();
+const Joi = require('joi');
+
+let genres = [
+    'Horror',
+    'Action',
+    'Comedy',
+    'Drama',
+    'History',
+    'Documentary'
+];
+
+router.get('/', (req, res) => {
+    return res.send(genres);
+});
+
+router.post('/', (req, res) => {
+    const result = validateGenre(req.body);
+    if (result.error) return res.status(400).send(result.error.details.slice().shift().message);
+    genres.push(req.body.name);
+    return res.send(genres);
+});
+
+router.put('/:id', (req, res) => {
+    const result = validateGenre(req.body);
+    if (result.error) return res.status(400).send(result.error.details.slice().shift().message);
+    const id = +req.params.id;
+    const genre = genres.find((g, index) => index === id);
+    if (genre) {
+       genres[id] = req.body.name;
+        return res.send(genres);
+    }
+    return res.status(404).send(`genre for ${id} not found`);
+});
+
+router.delete('/:id', (req, res) => {
+    const id = +req.params.id;
+    if (id > 0 && genres.length >= id) {
+        genres = genres.slice(id);
+        return res.send(genres);
+    }
+    return res.status(404).send(`genre for ${id} not found`);
+});
+
+const validateGenre = (genre) => {
+    const schema = {
+        name: Joi.string()
+            .min(3)
+            .required()
+    };
+    return Joi.validate(genre, schema);
+};
 
 module.exports = router;
