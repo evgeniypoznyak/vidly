deda5554d0bf1dff27b13ecd0769a2412596433f
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-02-24 00:59:23 -0600

    users simple post

--- models/user.js
+++ models/user.js
@@ -4,36 +4,35 @@ const Joi = require('joi');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        minlength: 2,
-        maxlength: 255,
+        minlength: 5,
+        maxlength: 50,
         required: true,
     },
     email: {
         type: String,
-        unique: true,
-        // createIndexes: true,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
     },
     password: {
         type: String,
         required: true,
+        minlength: 5,
+        maxlength: 1024,
+        unique: true
     },
 });
 const User = mongoose.model('Users', userSchema);
 
 const validateUser = (user) => {
     const schema = {
-        name: Joi.string()
-            .min(3)
-            .required(),
-        email: Joi.string()
-            .required(),
-        password: Joi.string()
-            .required()
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
     };
     return Joi.validate(user, schema);
 };
 
 exports.User = User;
-exports.userSchema = userSchema;
 exports.validate = validateUser;
